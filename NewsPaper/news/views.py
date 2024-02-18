5883d1f3995b54c6c0546f72cac885eb80b30680
ARSmobi <102361458+ARSmobi@users.noreply.github.com>
2024-02-19 00:48:28 +0300

    Add files via upload

---
+++ NewsPaper/news/views.py
@@ -0,0 +1,15 @@
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+
+class PostList(ListView):
+    model = Post
+    ordering = '-dateCreation'
+    template_name = 'news.html'
+    context_object_name = 'posts'
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
